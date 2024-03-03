48106758cd06ce93665e870716dcff409cf40f31
Mateusz Kubiak <matkubiak888@gmail.com>
2024-03-03 23:34:45 +0000

    test: HexColor::toStr()

--- test/UtilTest.cpp
+++ test/UtilTest.cpp
@@ -1,17 +1,31 @@
 #include <boost/test/unit_test.hpp>
+#include <algorithm>
 
 #include "Core/util.h"
 
 BOOST_AUTO_TEST_SUITE(UtilTest)
 
-BOOST_AUTO_TEST_CASE(UtilTest1) {
-
+BOOST_AUTO_TEST_CASE(HexToTupleTest) {
     std::tuple<int, int, int> aaa = HexColor::values("#3C281A");
     auto [red, green, blue] = aaa;
 
     BOOST_CHECK_EQUAL(red, 60);
     BOOST_CHECK_EQUAL(green, 40);
     BOOST_CHECK_EQUAL(blue, 26);
+}
+
+BOOST_AUTO_TEST_CASE(TupleToHexTest) {
+    int red = 60;
+    int green = 40;
+    int blue = 26;
+
+    std::string hex = "#3C281A";
+
+    std::string result = HexColor::toStr(red, green, blue);
+    std::transform(result.begin(), result.end(), result.begin(),
+        [](unsigned char c) { return std::toupper(c); });
+    
+    BOOST_CHECK_EQUAL(hex, result);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
