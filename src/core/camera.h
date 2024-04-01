004fe6da5427876438f6f848f1fb7397a4755880
Mateusz Kubiak <matkubiak888@gmail.com>
2024-04-01 12:19:28 +0200

    docs: camera

--- src/core/camera.h
+++ src/core/camera.h
@@ -1,6 +1,7 @@
 #ifndef CORE_CAMERA_H_
 #define CORE_CAMERA_H_
 
+/// @brief Basic perspective camera
 class Camera {
   glm::vec3 pos;
   float yaw, pitch;
@@ -8,12 +9,23 @@ class Camera {
  public:
   Camera();
 
+  /// @brief constructs a view matrix from the point of the camera
   glm::mat4 view() const;
 
+  /// @brief translates the camera
+  /// @param transf displacement vector
   inline void move(glm::vec3 transf) { pos += transf; }
+
+  /// @brief rotates the camera
+  /// @param yaw vertical angle in degrees
+  /// @param pitch horizontal angle in degrees
   void rotate(float yaw, float pitch);
 
+  /// @brief returns a normalized vector pointing in the direction the camera is
+  /// pointing
   glm::vec3 front() const;
+  
+  /// @brief returns a vector pointing upwards
   glm::vec3 up() const;
 };
 
